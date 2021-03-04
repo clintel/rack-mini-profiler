b383e6545a7a924a1dd2b7d48f151c505c5d5171
ben <ben@clintelsystems.com>
2021-03-04 15:57:16 +1030

    remove all the sql patches for native databases and leave just active record

--- lib/patches/sql_patches.rb
+++ lib/patches/sql_patches.rb
@@ -29,32 +29,13 @@ class SqlPatches
 
   def self.sql_patches
     patches = []
-
-    patches << 'mysql2' if defined?(Mysql2::Client) && Mysql2::Client.class == Class
-    patches << 'pg' if defined?(PG::Result) && PG::Result.class == Class
-    patches << 'oracle_enhanced' if defined?(ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter) && ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class == Class &&
-                                    SqlPatches.correct_version?('~> 1.5.0', ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter) &&
-                                    patch_rails?
-    # if the adapters were directly patched, don't patch again
-    if !patches.empty?
-      Rack::MiniProfiler.subscribe_sql_active_record = false
-      return patches
-    end
-    patches << 'sequel' if defined?(Sequel::Database) && Sequel::Database.class == Class
-    patches << 'activerecord' if defined?(ActiveRecord) && ActiveRecord.class == Module && patch_rails?
+    patches << 'activerecord' if defined?(ActiveRecord) && ActiveRecord.class == Module
     Rack::MiniProfiler.subscribe_sql_active_record = patches.empty? && !patch_rails?
     patches
   end
 
   def self.other_patches
     patches = []
-    patches << 'mongo' if defined?(Mongo::Server::Connection) && Mongo.class == Module
-    patches << 'moped' if defined?(Moped::Node) && Moped::Node.class == Class
-    patches << 'plucky' if defined?(Plucky::Query) && Plucky::Query.class == Class
-    patches << 'rsolr' if defined?(RSolr::Connection) && RSolr::Connection.class == Class && RSolr::VERSION[0] != "0"
-    patches << 'nobrainer' if defined?(NoBrainer) && NoBrainer.class == Module
-    patches << 'riak' if defined?(Riak) && Riak.class == Module
-    patches << 'neo4j' if defined?(Neo4j::Core) && Neo4j::Core::Query.class == Class
     patches
   end
 
